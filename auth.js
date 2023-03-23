ca3d4cbf581cd38ef63c133ae2c2945abbe028f4
MALWICK <batahumphrey66@gmail.com>
2023-03-23 17:30:41 +0100

    creating a function to check admin Users

--- auth.js
+++ auth.js
@@ -10,9 +10,7 @@ const loginWithEmailPassword = async (email, password) => {
   });
   console.log(user);
   if (!user) {
-    {
-      status: 401;
-    }
+    return { status: 401 };
   }
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
@@ -21,7 +19,7 @@ const loginWithEmailPassword = async (email, password) => {
 
   const token = jwt.sign({ user_id: user.id, email }, JWT_PRIVATE_KEY, {
     expiresIn: "2h",
-  })
+  });
 
   return { token, user };
 };
@@ -31,14 +29,11 @@ const verifyToken = async (token) => {
     return { status: 401 };
   }
   try {
-    const decoded = jwt.verify(token,JWT_PRIVATE_KEY);
+    const decoded = jwt.verify(token, JWT_PRIVATE_KEY);
 
     const user = await User.findByPk(user.id);
     if (!user) {
-      return
-      {
-        status: 401;
-      }
+      return { status: 401 }
     }
     return decoded;
   } catch (error) {
@@ -46,20 +41,67 @@ const verifyToken = async (token) => {
   }
 };
 
-
-const loginWithApi = async (apikey) => {
+const loginWithApi = async (apiKey) => {
   let user = await User.findOne({
     where: {
-      apikey: apikey,
-    }
+      apiKey: apiKey,
+    },
   });
 
-  user = user.dataValues
+  /*   user = user.dataValues; */
 
-  if(!user) return { status: 401 }
-  
-  return user
-}
+  if (!user) {
+    return { status: 401 }
+  };
+
+  return {user, apiKey};
+};
+
+const authorizeIfAdmin = async (token, func) => {
+  const user = await verifyToken(token);
+  const is_admin = user.is_admin;
+  if (!is_admin) {
+    return { status: 401 };
+  } else {
+    try {
+     return func(user);
+    } catch (error) {
+      return { status: 401 };
+    }
+  }
+  /* let role = await User.findByPk({
+    where: {
+      is_Admin: is_Admin,
+    },
+  })
+  if (role && token) {
+    if (role === "is_Admin") {
+      await User.findById(token);
+    } else {
+      return { status: 401 };
+    }
+  }else {
+    return { status: 401 , message: "Role or TOKEN not present"};
+  } */
+};
+
+const authorizeToken = async (token, func) => {
+  const user = await verifyToken(token);
+  return func(user);
+};
+
+const authorizeApi = async (apiKey, func) => {
+  const user = await loginWithApi(apiKey);
+  func(user);
+  return func;
+};
 console.log(verifyToken());
 
-module.exports = { loginWithEmailPassword, verifyToken, loginWithApi };
+module.exports = {
+  loginWithEmailPassword,
+  verifyToken,
+  loginWithApi,
+  authorizeIfAdmin,
+  authorizeToken,
+  authorizeApi,
+};
--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const relate = require("./database/relationships");
 relate();
 
 
-const {loginWithEmailPassword} = require("./auth");
+const {loginWithEmailPassword,
+    verifyToken,
+    loginWithApi,
+    authorizeIfAdmin,
+    authorizeToken,
+    authorizeApi,} = require("./auth");
 
 
 
-loginWithEmailPassword("jahmorant@gmail.com", "1234").then(user => console.log(user));
+loginWithEmailPassword("debra@gmail.com", "1234").then(user => console.log(user));
+
+
+// verifyToken( 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMiwiZW1haWwiOiJqYWhtb3JhbnRAZ21haWwuY29tIiwiaWF0IjoxNjc5NTYzNzMxLCJleHAiOjE2Nzk1NzA5MzF9.b_lAEQVMaaDyx7BHZHVgcEZ2cSp58FO_k0vzkWTiIWo').then((res) => console.log('this res', res))
+
+// loginWithApi('1').then(res => console.log('this api user', res))
+
+ authorizeIfAdmin( 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6ImRlYnJhQGdtYWlsLmNvbSIsImlhdCI6MTY3OTU3NzAxNSwiZXhwIjoxNjc5NTg0MjE1fQ.VBw4kksgdcj1HnUyqT1rPJQWicr0YFJ8DQW9hGQnumI').then((res) => console.log('this res', res))
+ 
